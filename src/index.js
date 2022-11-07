ca6d1f6f6d22386214130067cfcd0e6f20b8fd91
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-11-07 15:17:00 +0530

    import main css file and theme css file for using react-date-range

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
