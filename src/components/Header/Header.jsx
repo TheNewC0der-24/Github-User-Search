e34e1b849322b4186011ebf8ecda997500038317
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-11-07 14:18:37 +0530

    Create header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+import { ImGithub } from 'react-icons/im';
+
+const Header = () => {
+    return (
+        <>
+            <nav className={`${styles.nav} navbar navbar-dark`}>
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0 h1"><ImGithub className='fs-3 me-2' />Github User Search <span className={styles.description}>and List Sorting</span></span>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Header;
