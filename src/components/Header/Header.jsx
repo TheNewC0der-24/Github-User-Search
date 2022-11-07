a9c996acf45fec6f56799d09f4c3220b4238f4f4
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-11-07 15:45:36 +0530

    Make the header sticky

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import { ImGithub } from 'react-icons/im';
 const Header = () => {
     return (
         <>
-            <nav className={`${styles.nav} navbar navbar-dark`}>
+            <nav className={`${styles.nav} navbar sticky-top navbar-dark`}>
                 <div className="container-fluid">
                     <span className="navbar-brand mb-0 h1"><ImGithub className='fs-3 me-2' />Github User Search <span className={styles.description}>and List Sorting</span></span>
                 </div>
