3d7e35271dd1d7b55d959fae69c8cc06f53e758d
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-02-24 16:48:31 +0530

    Create UI to show Fetched Data

--- src/components/SearchedUser/SearchedUser.jsx
+++ src/components/SearchedUser/SearchedUser.jsx
@@ -1,152 +1,103 @@
 import React from 'react';
+import styles from './SearchUser.module.css';
 
-const SearchedUser = (props) => {
+import CountUp from 'react-countup';
+
+import {
+    Avatar,
+    Box,
+    Container,
+    Typography,
+    Link,
+    Grid,
+    Divider
+} from '@mui/material';
+
+import { FaStar, FaLink, FaExternalLinkAlt } from 'react-icons/fa';
+import { IoLocationSharp } from 'react-icons/io5';
+import { BiGitRepoForked } from 'react-icons/bi';
+import { HiOutlineBuildingOffice2 } from 'react-icons/hi2';
+
+const SearchedUser = ({ searchUser, userRepo, userFollowers }) => {
 
     return (
         <>
-            <div className={`${props.display} container my-3`}>
-                {props.searchUser.length > 1 ?
-                    props.searchUser.map(user => (
-                        <div>
-                            <h3 className='mb-3'>Searched User</h3>
-                            <div className="row row-cols-1 row-cols-lg-6 g-4">
-                                <div className="col">
-                                    <div className="card h-100">
-                                        <div className="card-body">
-                                            <h5 className="card-title">Name</h5>
-                                            <p className="card-text text-primary">
-                                                <a href={user.url} target="_blank" rel="noreferrer">
-                                                    {user.name}
-                                                </a>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col">
-                                    <div className="card h-100">
-                                        <div className="card-body">
-                                            <h5 className="card-title">Contributions</h5>
-                                            <p className="card-text text-primary">
-                                                100
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col">
-                                    <div className="card h-100">
-                                        <div className="card-body">
-                                            <h5 className="card-title">Followers</h5>
-                                            <p className="card-text text-primary">
-                                                {user.followers}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col">
-                                    <div className="card h-100">
-                                        <div className="card-body">
-                                            <h5 className="card-title">Repo Count</h5>
-                                            <p className="card-text text-primary">
-                                                {user.repo}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col">
-                                    <div className="card h-100">
-                                        <div className="card-body">
-                                            <h5 className="card-title">Location</h5>
-                                            <p className="card-text text-primary">
-                                                {user.location}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col">
-                                    <div className="card h-100">
-                                        <div className="card-body">
-                                            <h5 className="card-title">Bio</h5>
-                                            <p className="card-text text-primary">
-                                                {user.bio}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    )) : (
-                        <div>
-                            <h3 className='mb-3'>Searched User</h3>
-                            <div className="row row-cols-1 row-cols-lg-6 g-4">
-                                <div className="col">
-                                    <div className="card h-100">
-                                        <div className="card-body">
-                                            <h5 className="card-title">Name</h5>
-                                            <p className="card-text text-primary">
-                                                <a href={props.searchUser.url} target="_blank" rel="noreferrer">
-                                                    {props.searchUser.name ? props.searchUser.name : 'Not Found'}
-                                                </a>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col">
-                                    <div className="card h-100">
-                                        <div className="card-body">
-                                            <h5 className="card-title">Contributions</h5>
-                                            <p className="card-text text-primary">
-                                                Not Found
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col">
-                                    <div className="card h-100">
-                                        <div className="card-body">
-                                            <h5 className="card-title">Followers</h5>
-                                            <p className="card-text text-primary">
-                                                {props.searchUser.followers ? props.searchUser.followers : 'Not Found'}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col">
-                                    <div className="card h-100">
-                                        <div className="card-body">
-                                            <h5 className="card-title">Repo Count</h5>
-                                            <p className="card-text text-primary">
-                                                {props.searchUser.repo ? props.searchUser.repo : 'Not Found'}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col">
-                                    <div className="card h-100">
-                                        <div className="card-body">
-                                            <h5 className="card-title">Location</h5>
-                                            <p className="card-text text-primary">
-                                                {props.searchUser.location ? props.searchUser.location : 'Not Found'}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col">
-                                    <div className="card h-100">
-                                        <div className="card-body">
-                                            <h5 className="card-title">Bio</h5>
-                                            <p className="card-text text-primary">
-                                                {props.searchUser.bio ? props.searchUser.bio : 'No Bio'}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                }
-            </div>
+            <Container maxWidth="lg" sx={{ mt: 5 }}>
+                <Box className={styles.box}>
+                    <img className={styles.img} src={searchUser.avatar_url} alt={searchUser.name} />
+                    <Box sx={{ display: 'flex', flexDirection: "column", gap: "0.5rem" }}>
+                        <Box sx={{ display: 'flex', flexWrap: "wrap", flexDirection: "row", alignItems: "center", gap: "0.5rem" }}>
+                            <Typography variant='h5' sx={{ fontWeight: "bold" }}>{searchUser.name}</Typography>
+                            <Link href={searchUser.html_url} underline="hover" target="_blank">@{searchUser.login}</Link>
+                        </Box>
+                        <Typography variant='subtitle1'>{searchUser.bio}</Typography>
+
+                        <Box sx={{ mt: 3, mb: 3 }}>
+                            <Typography variant='subtitle1' sx={{ display: 'flex', alignItems: "center", gap: "0.5rem" }}><HiOutlineBuildingOffice2 />{searchUser.company ? searchUser.company : "NOT FOUND"}</Typography>
+                            <Typography variant='subtitle1' sx={{ display: 'flex', alignItems: "center", gap: "0.5rem" }}><IoLocationSharp />{searchUser.location ? searchUser.location : "NOT FOUND"}</Typography>
+                            <Link underline='hover' href={searchUser.blog} target="_blank" sx={{ display: 'flex', alignItems: "center", gap: "0.5rem" }}><FaLink style={{ color: "white" }} />{searchUser.blog ? searchUser.blog : "NOT FOUND"}</Link>
+                        </Box>
+
+                        <Box sx={{ display: "flex", gap: "2rem" }}>
+                            <Box sx={{ display: 'flex', flexDirection: "column" }}>
+                                <Typography variant='h4' sx={{ fontWeight: "bold", textAlign: "center" }}><CountUp end={searchUser.followers} /></Typography>
+                                <Typography variant='h6' sx={{ fontWeight: "bold" }}>Followers</Typography>
+                            </Box>
+                            <Box sx={{ display: 'flex', flexDirection: "column" }}>
+                                <Typography variant='h4' sx={{ fontWeight: "bold", textAlign: "center" }}><CountUp end={searchUser.following} /></Typography>
+                                <Typography variant='h6' sx={{ fontWeight: "bold" }}>Following</Typography>
+                            </Box>
+                            <Box sx={{ display: 'flex', flexDirection: "column" }}>
+                                <Typography variant='h4' sx={{ fontWeight: "bold", textAlign: "center" }}><CountUp end={searchUser.public_repos} /></Typography>
+                                <Typography variant='h6' sx={{ fontWeight: "bold" }}>Repos</Typography>
+                            </Box>
+                        </Box>
+                    </Box>
+                </Box>
+
+                <Grid container spacing={5} sx={{ mt: 3, mb: 3 }}>
+                    <Grid item xs={12} sm={12} md={6} lg={6}>
+                        <Typography mb={3} variant='h5'>Repositories</Typography>
+                        {userRepo.slice(0, 8).map((repo) => (
+                            <Box key={repo.id} sx={{ display: "flex", justifyContent: "space-between", flexDirection: "column" }}>
+                                <Box>
+                                    <Link href={repo.html_url} target="_blank" underline='hover' sx={{ fontSize: "20px" }}>{repo.name}</Link>
+                                    <Typography variant='subtitle1'>{repo.description}</Typography>
+                                </Box>
+                                <Box mt={5} sx={{ display: "flex", justifyContent: "space-between", flexDirection: "row", alignItems: "center" }}>
+                                    <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", gap: "1rem" }}>
+                                        <Typography variant='subtitle1'><FaStar /> {repo.stargazers_count}</Typography>
+                                        <Typography variant='subtitle1'><BiGitRepoForked /> {repo.fork === true ? "1" : "0"}</Typography>
+                                    </Box>
+                                    <Box sx={{ display: "flex", flexDirection: "row" }}>
+                                        <Link href={repo.homepage} sx={{ color: "#fff" }}>{repo.homepage ? <FaExternalLinkAlt /> : ""}</Link>
+                                    </Box>
+                                </Box>
+                                <Divider sx={{ backgroundColor: "#dee2e6", mt: 2, mb: 2 }} />
+                            </Box>
+                        ))}
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={6} lg={6}>
+                        <Typography mb={3} variant='h5'>Followers</Typography>
+                        {userFollowers.slice(0, 8).map((follower) => (
+                            <Box key={follower.id}>
+                                <Box sx={{ display: "flex", gap: "1rem" }}>
+                                    <Avatar
+                                        sx={{ width: 100, height: 100 }}
+                                        src={follower.avatar_url}
+                                        alt={follower.login}
+                                        variant="rounded"
+                                    />
+                                    <Link href={follower.html_url} underline="none" sx={{ fontSize: "20px", color: "#fff", '&:hover': { color: "#1976d2" } }} target="_blank">{follower.login}</Link>
+                                </Box>
+                                <Divider sx={{ backgroundColor: "#dee2e6", mt: 2, mb: 2 }} />
+                            </Box>
+                        ))}
+                    </Grid>
+                </Grid>
+            </Container>
         </>
     )
 }
 
-export default SearchedUser;
+export default SearchedUser; 
